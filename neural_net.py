3873ca39670cc66d8f4402abb83b911229988ef8
henriqueramalho1 <henrique.ramalho2002@gmail.com>
2023-12-07 14:29:49 -0300

    Código teste cego

--- neural_net.py
+++ neural_net.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
 import sys
+import csv
         
 class neural_net:
     def __init__(self):
@@ -58,51 +59,28 @@ class neural_net:
             tf.keras.layers.Dense(8, activation='relu'),
             tf.keras.layers.Dense(1)
         ])
-        model3.compile(optimizer='adam', loss='mean_squared_error')
+        model3.compile(loss='mean_squared_error')
 
-
-        m1 = KerasRegressor(model1, batch_size=32, epochs=100, verbose=0)
-        m2 = KerasRegressor(model2, batch_size=32, epochs=100, verbose=0)
-        m3 = KerasRegressor(model3, batch_size=32, epochs=100, verbose=0)
-
-        val_score1 = np.mean(cross_val_score(m1, X_train, T_train, cv=5))
-        val_score2 = np.mean(cross_val_score(m2, X_train, T_train, cv=5))
-        val_score3 = np.mean(cross_val_score(m3, X_train, T_train, cv=5))
-
-        max_cv_score = max (val_score1, val_score2, val_score3)
-
-        final_model = None
-
-        if max_cv_score == val_score1:
-           print("Modelo 1 escolhido")
-           final_model = m1
-        
-        if max_cv_score == val_score2:
-            print("Modelo 1 escolhido")
-            final_model = m2
-
-        if max_cv_score == val_score3:
-            print("Modelo 3 escolhido")
-            final_model = m3
-
-        final_model.fit(X_test, T_test, verbose = 1)
-        print("Modelo final treinado")
-        # P_test = model.predict(TESTE_CEGO)
-        # mean_squared_error(P_test, EXPECTED TESTE CEGO)
 
         # Treinando as redes
-        # model1.fit(X_train, T_train, epochs=100, batch_size=32, validation_data=(X_test, T_test), verbose=2)
-        # model2.fit(X_train, T_train, epochs=100, batch_size=32, validation_data=(X_test, T_test), verbose=2)
-        # model3.fit(X_train, T_train, epochs=100, batch_size=32, validation_data=(X_test, T_test), verbose=2)
+        model1.fit(X_train, T_train, epochs=100, batch_size=32, validation_data=(X_test, T_test), verbose=2)
+
+        # Fazendo previsões
+        sample_data = pd.read_csv('datasets/teste_cego/sinais_vitais_teste.txt',header=None)
+        print(sample_data)
+        print(f"Tamanho da entrada = {len(sample_data)}")
+        sample_data = sample_data.iloc[:, 1:4].values  # Utilizando apenas as colunas 3, 4 e 5 (pressão, pulso, respiração)
+        print(f"Tamanho da entrada filtrada = {len(sample_data)}")
+        sample_data = scaler.transform(sample_data)
         
-        # Avaliando os modelos
-        # loss1 = model1.evaluate(X_test, T_test, verbose=0)
-        # print(f'Mean Squared Error on Test Data (Model 1): {loss1}')
-        
-        # loss2 = model2.evaluate(X_test, T_test, verbose=0)
-        # print(f'Mean Squared Error on Test Data (Model 2): {loss2}')
-        
-        # loss3 = model3.evaluate(X_test, T_test, verbose=0)
-        # print(f'Mean Squared Error on Test Data (Model 3): {loss3}')
-        
+        predicted_gravity1 = model1.predict(sample_data)
+        print(f"Tamanho da saida = {len(predicted_gravity1)}")
+        output = 'rn.txt'
+
+        # Abrir o arquivo em modo de escrita
+        with open(output, 'w') as file:
+            # Escrever cada elemento do vetor em uma linha
+            writer = csv.writer(file)
+            writer.writerows(predicted_gravity1)
+
         sys.exit()
